566f8a1edcbb8e2509eab7f64f389ffbe6aca490
Mukul1945 <mukulchauhan1256@gmail.com>
2026-01-14 22:53:29 +0530

    changes

--- Array/Reverse.cpp
+++ Array/Reverse.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World";
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int start = 0;
+    int end = n - 1;
+
+    while (start < end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+
+    cout << "Reversed array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
