f96e25c04bf58ab59cf46dea32e617c81857f60c
Mukul1945 <mukulchauhan1256@gmail.com>
2026-01-15 11:53:18 +0530

    dsa problem

---
+++ Array/Sort_0_1_2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int start = 0;
+    int mid = 0;
+    int end = n - 1;
+    while (mid <= end) {
+        if (arr[mid] == 0) {
+            swap(arr[start], arr[mid]);  
+            start++;
+            mid++;
+        }
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+        else { 
+            swap(arr[mid], arr[end]);
+            end--;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
