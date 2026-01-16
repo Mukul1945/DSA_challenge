15a972264bee63d09ed3118496b646ef57fd236e
Mukul1945 <mukulchauhan1256@gmail.com>
2026-01-16 22:37:52 +0530

    changes

--- Array/Move_Negative.cpp
+++ Array/Move_Negative.cpp
@@ -1,13 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+
+int main() {
     int n;
-    cin>>n;
+    cin >> n;
+
     int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
 
-    int start=0;
-    
+    int left = 0;
+    int right = n - 1;
+
+    while(left <= right) {
+        if(arr[left] < 0) {
+            left++;
+        }
+        else if(arr[right] >= 0) {
+            right--;
+        }
+        else {
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
+        }
+    }
+
+    // Output
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
 }
