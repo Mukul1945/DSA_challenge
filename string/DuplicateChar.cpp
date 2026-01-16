a488b7750c02539cf06f365ecbac4bdc37982111
Mukul1945 <mukulchauhan1256@gmail.com>
2026-01-16 22:39:42 +0530

    changes

--- string/DuplicateChar.cpp
+++ string/DuplicateChar.cpp
@@ -1,13 +1,51 @@
-#include<iostream>
+// #include <iostream>
+// #include <map>
+// using namespace std;
+
+// int main() {
+//     string s;
+//     cin >> s;
+
+//     map<char, int> mp;
+//     for(char ch : s) {
+//         mp[ch]++;
+//     }
+
+//     for(auto it : mp) {
+//         if(it.second > 1) {
+//             cout << it.first << " ";
+//         }
+//     }
+
+//     return 0;
+// }
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     int n;
-    cin>>n;
+    cin >> n;
+
     string s[n];
-    for(int i=0;i<n;i++){
-        cin>>s[n];
+    for(int i = 0; i < n; i++) {
+        cin >> s[i];
     }
 
-    
+    for(int i = 0; i < n; i++) {
+        int freq[256] = {0};
+
+        for(char ch : s[i]) {
+            freq[ch]++;
+        }
+
+        cout << "Duplicate characters in \"" << s[i] << "\": ";
+        for(int j = 0; j < 256; j++) {
+            if(freq[j] > 1) {
+                cout << (char)j << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
